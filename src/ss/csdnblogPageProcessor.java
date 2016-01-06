1de671bb0fd7154be638e03123da7f22ae7b3806
Pku <pkusser@sina.cn>
2016-01-06 10:57:53 +0800

    Architecture

--- src/ss/csdnblogPageProcessor.java
+++ src/ss/csdnblogPageProcessor.java
@@ -1,10 +1,5 @@
 package ss;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,98 +8,83 @@ import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.processor.PageProcessor;
 import us.codecraft.webmagic.selector.Selectable;
-import us.codecraft.webmagic.utils.UrlUtils;
 
 public class csdnblogPageProcessor implements PageProcessor {
-    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000).setTimeOut(2000);
-    
-	public void process(Page page) {
-		
-		try {
-   
-		String articlePattern="http://blog.csdn.net/\\w+/article/details/\\d+";
-		String expertPattern="\\bhttp://blog.csdn.net/\\w+(?!/)\\b";
-		String exn="(" + expertPattern.replace(".", "\\.").replace("*", "[^\"'#]*") + ")";
-		Selectable url = page.getUrl();	
-		if (url.regex("http://blog.csdn.net/index.html").match()) //首页
-		{
-			//article
-			addRequests(page, articlePattern);
-			//expert		
-			addRequests(page, expertPattern);
-//			//category
-			List<String> category_urls = page.getHtml().css("div.side_nav").links().all();
-			page.addTargetRequests(category_urls);
-//			//next page
-			List<String> page_urls = page.getHtml().css("div.page_nav").links().all();
-			page.addTargetRequests(page_urls);
-//			String lastpage_url=page_urls.get(page_urls.size()-1);
-			// "/?&page=17"
-//			String[] analysis=lastpage_url.split("=");
-//			Integer page_count=Integer.parseInt(analysis[analysis.length-1]);
-//			addRequests(page, nextPagePattern);
+	private Site site = Site.me().setRetryTimes(3).setSleepTime(1000).setTimeOut(6000);
 
-		}
-		//article
-		else if (url.regex(articlePattern).match())
-		{
-//			System.out.println("this is article");
-			//save article
-			String cur_title=page.getHtml().xpath("//h1/span/a/text()").toString();
-			String cur_user=page.getHtml().xpath("//div[@id='blog_userface']/span/a[@class='user_name']/text()").toString();
-			Article newarticle=new Article(cur_user, cur_title);
-			SQLiteHelper.mySqLiteHelper.addArticle(newarticle);
-			
-//			page.putField("author", url.toString());
-			//add user
-			String url_str=url.toString();
-			Pattern p= Pattern.compile("http://blog.csdn.net/\\w+"); 
-			Matcher m = p.matcher(url_str);
-			if(m.find())
+	public void process(Page page) {
+
+		try {
+
+			String initPage="http://blog.csdn.net/index.html";
+			String articlePattern = "http://blog.csdn.net/\\w+/article/details/\\d+";
+			String expertPattern = "\\bhttp://blog.csdn.net/\\w+(?!/)\\b";
+			String listpagePattern = "http://blog.csdn.net/\\w+/article/list/\\d+";
+			String exn = "(" + expertPattern.replace(".", "\\.").replace("*", "[^\"'#]*") + ")";
+			Selectable url = page.getUrl();
+			if (url.regex(initPage).match()) // index page
 			{
-				String user_url=m.group();
-//				System.out.println("==========");
-//				System.out.println(user_url);
-//				System.out.println("==========");
-				page.addTargetRequest(user_url);
+				// article
+				addRequests(page, articlePattern);
+				// expert
+				addRequests(page, expertPattern);
+				// //category
+				List<String> category_urls = page.getHtml().css("div.side_nav").links().all();
+				page.addTargetRequests(category_urls);
+				// //next page
+				List<String> page_urls = page.getHtml().css("div.page_nav").links().all();
+				page.addTargetRequests(page_urls);
+				// String lastpage_url=page_urls.get(page_urls.size()-1);
+				// "/?&page=17"
+				// String[] analysis=lastpage_url.split("=");
+				// Integer
+				// page_count=Integer.parseInt(analysis[analysis.length-1]);
+				// addRequests(page, nextPagePattern);
+
 			}
-			
-		}
-		
-		//user
-		else if (url.regex(exn).match())
-		{
-			System.out.println("this is expert");
-			List<String> page_urls = page.getHtml().css("span.link_view").links().all();
-			page.addTargetRequests(page_urls);
-		}
-		else {
-			
-		}
-//		if url.rege()
-		//age.getHtml().xpath("");
+			// article
+			else if (url.regex(articlePattern).match()) {
+				
+				WorkCenter.singleworkcenter.AddArticlePage(page);
+//				page.putField("url", url.toString());
+				// add user
+				String url_str = url.toString();
+				Pattern p = Pattern.compile("http://blog.csdn.net/\\w+");
+				Matcher m = p.matcher(url_str);
+				if (m.find()) {
+					String user_url = m.group();
+					page.addTargetRequest(user_url);
+				}
+
+			}
+
+			// user
+			else if (url.regex(exn).match() || url.regex(listpagePattern).match()) {
+				System.out.println("this is expert");
+				// get article list
+				List<String> article_urls = page.getHtml().css("span.link_view").links().all();
+				page.addTargetRequests(article_urls);
+				// get pagelist
+				List<String> page_urls = page.getHtml().css("div.pagelist").links().all();
+				page.addTargetRequests(page_urls);
+
+			} else {
+				//no match
+			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
-	public void addRequests(Page page,String urlPattern) {
-		String formatPattern="(" + urlPattern.replace(".", "\\.").replace("*", "[^\"'#]*") + ")";
-		List<String> requests = page.getHtml().links().regex(formatPattern).all();	
+	public void addRequests(Page page, String urlPattern) {
+		String formatPattern = "(" + urlPattern.replace(".", "\\.").replace("*", "[^\"'#]*") + ")";
+		List<String> requests = page.getHtml().links().regex(formatPattern).all();
 		page.addTargetRequests(requests);
 	}
+
 	@Override
 	public Site getSite() {
-		// TODO Auto-generated method stub
 		return site;
-	}
-
-	public void testurl(Selectable url) {
-		Selectable current=url.regex("index.html");
-		String bString=current.toString();
-		System.out.println(current.match());
 	}
 }
